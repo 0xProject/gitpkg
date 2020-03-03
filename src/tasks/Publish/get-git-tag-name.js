0c9538000c5a09488665435f5dbe1110e77f3148
fragosti <francesco.agosti93@gmail.com>
2020-03-03 13:16:12 -0800

    Add short git SHA to end of version being published

--- src/tasks/Publish/get-git-tag-name.js
+++ src/tasks/Publish/get-git-tag-name.js
@@ -1,6 +1,8 @@
+import { execSync } from 'child_process';
 import { normalisePackageNameNpm } from './normalise-package-name';
 
 export default function getGitTagName(pkg) {
-  const gitpkgPackageName = `${normalisePackageNameNpm(pkg.name)}-v${pkg.version}-gitpkg`;
+  const latestGitSha = execSync('git rev-parse --short HEAD').toString().trim();
+  const gitpkgPackageName = `${normalisePackageNameNpm(pkg.name)}-v${pkg.version}-${latestGitSha}`;
   return gitpkgPackageName;
 }
