5a886dbf4ed0254b26901a475dc3876a7824bf6f
fragosti <francesco.agosti93@gmail.com>
2020-03-03 13:18:56 -0800

    Add build files to repo

---
+++ build/tasks/Publish/get-git-tag-name.js
@@ -0,0 +1,21 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports.default = getGitTagName;
+
+var _child_process = require('child_process');
+
+var _normalisePackageName = require('./normalise-package-name');
+
+function getGitTagName(pkg) {
+  const latestGitSha = (0, _child_process.execSync)('git rev-parse --short HEAD').toString().trim();
+  const gitpkgPackageName = `${(0, _normalisePackageName.normalisePackageNameNpm)(
+    pkg.name
+  )}-v${pkg.version}-${latestGitSha}`;
+  return gitpkgPackageName;
+}
+
+console.log(getGitTagName({ name: 'asset-swapper', version: '1.2.3' }));
+//# sourceMappingURL=get-git-tag-name.js.map
